c2dc624f09754522e3e1f51d228602f43dc0cd58
Karthik D <karthik947@gmail.com>
2020-06-05 15:49:09 +0530

    Updated the code to make it compatible with the new Twitter UI

--- run.js
+++ run.js
@@ -1,9 +1,12 @@
 const cheerio = require('cheerio');
 const request = require('request');
 const notifier = require('node-notifier');
-var separateReqPool = {maxSockets: 15};
-var async = require('async');
-var tweets={},apiurls=[],N=[];
+const separateReqPool = {maxSockets: 15};
+const async = require('async');
+let tweets={},apiurls=[];
+const E = require('events');
+const trigger = new E();
+
 
 
 ///////////////////////////  CONFIGURE TWITTER HANDLERS /////////////////////////////////////////////////////
@@ -12,12 +15,12 @@ var THandlers=[
         name:'WhaleWatch',
         url:"https://twitter.com/whalewatchio?lang=en",
         keywords:"long",
-    }
+    },
 ];
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-
+console.log('Twitter => Desktop Notifications program is running');
 
 
 //ADD TWEETS
@@ -26,60 +29,39 @@ THandlers.forEach((th,i) => {
     apiurls.push(th.url);
 });
 
+const headers = {'user-agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36'};
 
 //MONITOR
 setInterval(() => {
     async.map(apiurls, function(item, callback){
-        request({url: item, pool: separateReqPool}, function (error, response, body) {
+        request({url: item,headers, pool: separateReqPool}, function (error, response, body) {
             try {
                 const $ = cheerio.load(body);
-                var turl = "https://twitter.com" + response.req.path;
+                const turl = "https://twitter.com" + response.req.path;
+                const title = THandlers.filter((d,i) => d.url === turl)[0].name;
                 if(!tweets[turl].length){
-                    //FIRST LOAD
-                    $('div.js-tweet-text-container p').each((i,e) => {
-                        let s_tweet = '';
-                        e.children.forEach((d,j) => {
-                            if(d.type === 'text'){
-                               s_tweet += ' ' + d.data;
-                            }
-                        });
-                        tweets[turl].push(s_tweet);
-                    });
+                  //FIRST LOAD
+                  tweets[turl] = $('div.tweet-text').toArray().map(d => { const t = $(d).text(); return t.substring(9,t.length-6)});
                 }
                 else{
                     //EVERY OTHER TIME
-                    $('div.js-tweet-text-container p').each((i,e) => {
-                        let s_tweet = '';
-                        e.children.forEach((d,j) => {
-                            if(d.type === 'text'){
-                               s_tweet += ' ' + d.data;
-                            }
-                        });
-                        //CHECK IF TWEET IS NEWS
-                        if(tweets[turl].indexOf(s_tweet) === -1){
-                            tweets[turl].push(s_tweet);
-                            let th_kw = THandlers.filter((d,i) => d.url === turl)[0].keywords.split(',');
-                            let th_name = THandlers.filter((d,i) => d.url === turl)[0].name;
-                            th_kw.forEach((kw,j) => {
-                                if(kw === '*'){
-                                    N.push({
-                                        tweet:s_tweet,
-                                        name:th_name
-                                    });
-                                }
-                                else{
-                                   if(s_tweet.indexOf(kw) != -1){
-                                        N.push({
-                                            tweet:s_tweet,
-                                            name:th_name
-                                        });
-                                    }
-                                }
-                            });
-                        }
+                    const newTweets = $('div.tweet-text').toArray().map(d => { const t = $(d).text(); return t.substring(9,t.length-6)});
+                    const slicelen = newTweets.map(d => {return tweets[turl].indexOf(d);}).indexOf(0);
+                    const ntweets = slicelen === -1 ? [] : newTweets.slice(0,slicelen);
+
+                    ntweets.filter(d => {
+                      const th_kw = THandlers.filter((d,i) => d.url === turl)[0].keywords.split(',');
+                      if(th_kw.includes('*')){
+                        return true;
+                      } else{
+                        const checkTexts = th_kw.map(kw => d.includes(kw) ? true : false);
+                        return checkTexts.includes(false) ? false : true;
+                      }
+                    }).forEach(message => {
+                      trigger.emit('NEWTWEET',{title,message});
                     });
-                }           
-                 
+                    tweets[turl] = newTweets;
+                }
             } catch (e) {
                   console.log('Error =>' + e);
             }
@@ -89,13 +71,11 @@ setInterval(() => {
     });
 },5*1000);//RUNS EVERY 5 SECONDS
 
-setInterval(() => {
-    if(N.length){
-        let n = N.shift();
-        notifier.notify({title: n.name,message: n.tweet,sound: true},
-          function(err, response) {
-            // Response is response from notification
-          }
-        );
+trigger.on('NEWTWEET',({title,message}) => {
+  notifier.notify({title,message,sound: true},
+    (err, response) => {
+      // Response is response from notification
+      if(err) console.log(err);
     }
-},500);
+  );
+});
